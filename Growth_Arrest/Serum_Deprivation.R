6dfe99541aa6c33b220720111df692b418b0ea3c
jamieendicott <40669241+jamieendicott@users.noreply.github.com>
2022-07-25 14:54:47 -0400

    Update Serum_Deprivation.R

--- Growth_Arrest/Serum_Deprivation.R
+++ Growth_Arrest/Serum_Deprivation.R
@@ -1,11 +1,16 @@
 library(ggplot2)
 library(sesame)
+library(lme4)
+library(multcomp)
 
 #serum withdrawal
 betas<-read.csv('ga.sw.all.betas.csv',row.names=1,check.names = FALSE)
+samples<-read.csv('samples.csv',row.names=1)
 s<-subset(samples,samples$characteristics..subexperiment=="Growth arrest: Serum withdrawal")
-b<-subset(betas,rownames(betas)%in%CpGs$V4)
+
+b<-subset(betas,rownames(betas)%in%EPIC.comPMD$V4)
 b<-b[,c(match(rownames(s),colnames(b)))]
+
 s$med<-apply(b,2,median,na.rm=T)
 levels(s$characteristics..culture_pctFBS)<-c('0.50%','1%','5%','15%')
 
@@ -16,7 +21,7 @@ serumwd.PDL<-ggplot(data=s,aes(x=characteristics..population_doublings,
   scale_color_manual(name="% Serum",values=c('0.50%'="deeppink4",'1%'="tomato3",'5%'="darkorange3",'15%'="goldenrod2"))+
   labs(y="Median PMD solo-WCGW methylation",x="Population Doublings")
 
-summary(lm(s$med~s$Total.PDL))
+summary(lm(s$med~s$characteristics..population_doublings))
 #Multiple R-squared:  0.775
 #p-value: 3.081e-10
 
@@ -36,9 +41,9 @@ serumwd.gc<-ggplot(data=s,aes(y=characteristics..population_doublings,
   labs(x="Days in Culture",y="Population Doublings")+geom_line(stat="smooth", method="lm",alpha=0.4)
 
 
-samples$m.med<-BetaValueToMValue(samples$med)
-samples$condition<-as.factor(samples$characteristics..culture_pctFBS)
-s<-samples
+s$m.med<-BetaValueToMValue(s$med)
+s$condition<-as.factor(s$characteristics..culture_pctFBS)
+
 res<-lmer(data=s,m.med ~ characteristics..days_in_culture+condition+(1|condition),REML = FALSE)
 anova(res)
 summary(glht(res,mcp(condition="Tukey")))
