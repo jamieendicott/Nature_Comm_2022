d439c753ac20c10377ecacb423e5271f667781dc
jamieendicott <40669241+jamieendicott@users.noreply.github.com>
2022-07-26 10:22:19 -0400

    Update 2_ImportProcessedEPIC.R

--- 2_ImportProcessedEPIC.R
+++ 2_ImportProcessedEPIC.R
@@ -2,9 +2,8 @@ library(GEOquery)
 Sys.setenv("VROOM_CONNECTION_SIZE" = 131072 * 100)
 g<-getGEO('GSE197512')
 p<-pData(g[[1]])
+g.data <- as.data.frame(exprs(g[[1]]))
+head(g.data)
+dim(g.data)
+
 p<-read.csv('samples.csv',row.names=1)
-
-getGEOSuppFiles(GEO='GSE197512', makeDirectory = TRUE, baseDir = getwd(),
-  fetch_files = TRUE) 
-list.files(".")
-
