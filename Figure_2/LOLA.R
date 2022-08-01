de6a72d498a4444c54f5a3802565f6486a80e402
jamieendicott <40669241+jamieendicott@users.noreply.github.com>
2022-08-01 11:03:54 -0400

    Update LOLA.R

--- Figure_2/LOLA.R
+++ Figure_2/LOLA.R
@@ -1,16 +1,33 @@
 library(transcripTools)
 library(dplyr)
 library(ggplot2)
-library("LOLA")
-library('GenomicRanges')
+library(LOLA)
+library(GenomicRanges)
 library(simpleCache)
 
-s<-subset(p,p$"subexperiment:ch1"=="TERT immortalization"&
-            p$"expression_vector:ch1"=="TERT")
-dim(s)
-s<-s[order(s$"population_doublings:ch1"),]
+#load methylation data
+library(GEOquery)
+library(purrr)
+Sys.setenv("VROOM_CONNECTION_SIZE" = 131072 * 100)
+g<-getGEO('GSE197512')
+p<-pData(g[[1]])
+betas <- as.data.frame(exprs(g[[1]]))
+dim(betas)
+#[1] 865918    372
+#beautify pdata
+p<-p[,c(1,2,48:56,58:60)]
+cols<-(as.character(map(strsplit(colnames(p), split = ":"), 1)))
+colnames(p)<-cols
 
+s<-subset(p,p$subexperiment=="TERT immortalization"&
+            p$expression_vector=="TERT")
+dim(s)
+s<-s[order(s$population_doublings),]
 b<-betas[,c(match(rownames(s),colnames(betas)))]
+#subset betas to PMD solo-WCGWs
+download.file('https://zwdzwd.s3.amazonaws.com/pmd/EPIC.comPMD.probes.tsv','./EPIC.comPMD.probes.tsv')
+EPIC.comPMD<-read.delim('EPIC.comPMD.probes.tsv',header=F)
+b<-subset(b,rownames(b)%in%EPIC.comPMD$V4)
 dim(b)
 #[1] 26732    65
 b$delta<-b[,ncol(b)]-b[,1]
@@ -28,17 +45,15 @@ v2<-subset(var.me,var.me$ile==2)
 v3<-subset(var.me,var.me$ile==3)
 v4<-subset(var.me,var.me$ile==4)
 
-U<-read.delim('soloWCGW.universe.bed',header=F)
-regionDB<-loadRegionDB('regions/LOLACore/hg19')
-setwd('/secondary/projects/laird/jamie/LOLA/AG06561/')
-CpGs<-read.table('EPIC.comPMD.probes.tsv')
-#convert to granges object, can also be granges list
+regionDB<-loadRegionDB('regions/LOLACore/hg19') 
+#create universe (here PMD solo-WCGWs on EPIC array)
+U<-EPIC.comPMD[,1:3]
+#convert to granges object
 userUniverse<-GRanges(seqnames = U$V1,
                       ranges = IRanges(start = U$V2,
                                        end = U$V3))
-
-group<-v4 #etc
-input<-subset(CpGs,CpGs$V4%in%rownames(group))
+group<-v1 #etc
+input<-subset(EPIC.comPMD,EPIC.comPMD$V4%in%rownames(group)) 
 input<-input[,1:3]
 userSets<-GRanges(seqnames = input$V1,
                   ranges = IRanges(start = input$V2,
@@ -46,8 +61,8 @@ userSets<-GRanges(seqnames = input$V1,
 locResults = runLOLA(userSets, userUniverse, regionDB, cores=1)
 head(locResults)
 
-writeCombinedEnrichment(locResults, outFolder= "lolaRes.TERT.v4")
-E<-read.delim('allEnrichments.v4.tsv')#etc
+writeCombinedEnrichment(locResults, outFolder= paste0(group,".lolaRes"))
+E<-read.delim('allEnrichments.tsv')#etc
 #select top 10
 E<-E[1:10,c(2,4,5,15)]
 #annotate description w dbset
@@ -60,7 +75,7 @@ E$group<-as.factor(E$group)
 E$Set<-reorder(E$Set, E$pValueLog,mean)
 
 g<-ggplot(data=E,aes(x=pValueLog,y=Set,fill=oddsRatio))
-pdf('TERT.enrichment.LOLA.pdf',width = 8,height=8)
+pdf('enrichment.LOLA.pdf',width = 8,height=8)
 g+geom_col()+theme_bw()+
   xlab('-log(p-val)')+
   scale_fill_distiller(palette = "Spectral")+
