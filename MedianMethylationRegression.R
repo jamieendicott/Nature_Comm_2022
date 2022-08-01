7fc69d4c6c4a0f1112f58bb9512a10b217914a72
jamieendicott <40669241+jamieendicott@users.noreply.github.com>
2022-08-01 11:38:51 -0400

    Update MedianMethylationRegression.R

--- MedianMethylationRegression.R
+++ MedianMethylationRegression.R
@@ -1,14 +1,26 @@
 library(ggplot2)
-
-#download manifest of PMD solo-WCGWs
+#load methylation data
+library(GEOquery)
+library(purrr)
+Sys.setenv("VROOM_CONNECTION_SIZE" = 131072 * 100)
+g<-getGEO('GSE197512')
+p<-pData(g[[1]])
+betas <- as.data.frame(exprs(g[[1]]))
+dim(betas)
+#[1] 865918    372
+#subset betas to PMD solo-WCGWs
 download.file('https://zwdzwd.s3.amazonaws.com/pmd/EPIC.comPMD.probes.tsv','./EPIC.comPMD.probes.tsv')
 EPIC.comPMD<-read.delim('EPIC.comPMD.probes.tsv',header=F)
-
-betas<-read.csv('processed.betas.csv',check.names=F,row.names=1) 
-#subset betas to only PMD solo-WCGWs
 b<-subset(betas,rownames(betas)%in%EPIC.comPMD$V4)
-samples<-read.csv('samples.csv')
-samples<-subset(samples,samples$characteristics..subexperiment=="Baseline profiling")
+dim(b)
+#[1] 26732   372
+
+#beautify pdata
+p<-p[,c(1,2,48:56,58:60)]
+cols<-(as.character(map(strsplit(colnames(p), split = ":"), 1)))
+colnames(p)<-cols
+
+samples<-subset(p,p$subexperiment=="Baseline profiling")
 b<-b[,c(match(rownames(samples),colnames(b)))]
 samples$med<-apply(b,2,median,na.rm=T)
 
@@ -17,7 +29,7 @@ cols<-c(AG21837="coral",AG06561="brown",
         AG16146="goldenrod",AG21839="darkslateblue",
         AG21859="darkslategray3")
 
-g.PDL<-ggplot(data=samples,aes(x=characteristics..population_doublings,y=med,col=characteristics..CORIELL_ID))+
+g.PDL<-ggplot(data=samples,aes(x=population_doublings,y=med,col=coriell_id))+
   geom_point(alpha=0.4)+geom_smooth(method = 'lm')+
   scale_color_manual(values=cols)+theme_classic()+
   labs(x="Population doublings in culture",y="Median PMD solo-WCGW Methylation")+
