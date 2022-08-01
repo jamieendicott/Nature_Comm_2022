22b4760c5518b533a4d3bb980540d23e9e7cd661
jamieendicott <40669241+jamieendicott@users.noreply.github.com>
2022-08-01 11:17:49 -0400

    Update TripleSankey.R

--- Figure_2/TripleSankey.R
+++ Figure_2/TripleSankey.R
@@ -1,18 +1,40 @@
-#Timepoint 1: Low-PD, pre-immortalization
-cell.line<-"AG06561"
-s2<-subset(p,p$"subexperiment:ch1"=="Baseline profiling"&
-            p$"coriell_id:ch1"==cell.line&
-            p$"subculture:ch1"=="b")
-dim(s2)
-s2<-s2[order(s2$"population_doublings:ch1"),]
-b2<-betas[,c(match(rownames(s2),colnames(betas)))]
+#load methylation data
+library(GEOquery)
+library(purrr)
+library(ggplot2)
+library(ggforce)
+Sys.setenv("VROOM_CONNECTION_SIZE" = 131072 * 100)
+g<-getGEO('GSE197512')
+p<-pData(g[[1]])
+betas <- as.data.frame(exprs(g[[1]]))
+dim(betas)
+#[1] 865918    372
+#beautify pdata
+p<-p[,c(1,2,48:56,58:60)]
+cols<-(as.character(map(strsplit(colnames(p), split = ":"), 1)))
+colnames(p)<-cols
+#subset betas to PMD solo-WCGWs
+download.file('https://zwdzwd.s3.amazonaws.com/pmd/EPIC.comPMD.probes.tsv','./EPIC.comPMD.probes.tsv')
+EPIC.comPMD<-read.delim('EPIC.comPMD.probes.tsv',header=F)
+b<-subset(betas,rownames(betas)%in%EPIC.comPMD$V4)
+dim(b)
+#[1] 26732   372
 
-#Timepoints 2 and 3: post-immortalization
-s<-subset(samples,samples$Experiment=="TERT immortalization"&
-            samples$meetsQC=='TRUE'&
-            samples$Expresison.vector=='TERT')
+#Timepoint 1: Low-PD, pre-immortalization
+#Timepoint 2: High-PD, pre-immortalization
+cell.line<-"AG06561"
+s2<-subset(p,p$subexperiment=="Baseline profiling"&
+            p$coriell_id==cell.line&
+            p$subculture=="b")
+dim(s2)
+s2<-s2[order(s2$population_doublings),]
+b2<-b[,c(match(rownames(s2),colnames(b)))]
+
+#Timepoint 3: post-immortalization
+s<-subset(p,p$subexperiment=="TERT immortalization"&
+            p$expression_vector=='TERT')
 dim(s)
-s<-s[order(s$"population_doublings:ch1"),]
+s<-s[order(s$population_doublings),]
 b<-b[,c(match(rownames(s),colnames(b)))]
 
 s3<-rbind(s2[1,],s2[nrow(s2),],s[nrow(s),])
@@ -38,8 +60,7 @@ b3$last<-as.factor(b3$last)
 b3<-na.omit(b3)
 
 dat<-b3[,4:6]
-dat<-me[,c(4:6,8)]
-data <- gather_set_data(dat, 1:4)
+data <- gather_set_data(dat, 1:3) 
 data$x <- factor(data$x, levels=c("first", "mid", "last","class"))
 
 pdf('parsets.pdf')
