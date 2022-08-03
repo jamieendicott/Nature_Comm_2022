006e8e142ae5f04f25ccde03e1039aab2740d12c
jamieendicott <40669241+jamieendicott@users.noreply.github.com>
2022-08-03 08:51:17 -0400

    Update MethyltransferaseExpression.R

--- RNAseq/MethyltransferaseExpression.R
+++ RNAseq/MethyltransferaseExpression.R
@@ -3,6 +3,10 @@
 #import RNAseq data
 library(GEOquery)
 library(purrr)
+library(reshape2)
+library(ggplot2)
+
+
 Sys.setenv("VROOM_CONNECTION_SIZE" = 131072 * 100)
 g<-getGEO('GSE197471')
 p<-(pData(g[[1]]))
@@ -26,8 +30,29 @@ rownames(res)<-res$Symbol
 res<-res[,-(1:4)]
 
 #normalize to PCNA
-tres<-t(res)
 PCNA<-as.numeric(subset(res,rownames(res)=="PCNA"))
+PCNA.norm<-apply(res,1,function(x) x/PCNA)
 
-PCNA.norm<-apply(tres,1,function(x) x/PCNA)
-PCNA.norm<-apply(res,2,function(x) x/PCNA)
+#vis
+cols<-c(AG06561="brown",
+        AG11182="plum4",AG11546="darkseagreen4",
+        AG16146="goldenrod",AG21839="darkslateblue",
+        AG21859="darkslategray3")
+                 
+dat<-p[,c(4,10)]
+rownames(dat)<-p$description
+dat<-cbind(dat,PCNA.norm)        
+mdat<-melt(dat,id.vars = c('population_doublings','coriell_id'))
+mdat$population_doublings<-as.numeric(mdat$population_doublings)
+mdat$coriell_id<-as.factor(mdat$coriell_id)
+                 
+g<-ggplot(data=mdat,aes(x=population_doublings,y=value,col=coriell_id))
+pdf('methyltransferase.exprs.pdf',width = 9,height = 3)
+g+geom_point(alpha=0.5)+
+  scale_color_manual(values=cols)+theme_bw()+
+  labs(x="Population doublings in culture",y="log2(cpm+1)")+
+  facet_wrap(~variable,ncol=7)+
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+dev.off()
+                 
+                 
