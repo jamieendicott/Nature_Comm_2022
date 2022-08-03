c3a98630a9aec735b0490437d96dc2f4897c3770
jamieendicott <40669241+jamieendicott@users.noreply.github.com>
2022-08-03 10:23:25 -0400

    Create SessionInfo.R

---
+++ SessionInfo.R
@@ -0,0 +1,40 @@
+sessionInfo()
+
+R version 4.1.3 (2022-03-10)
+Platform: x86_64-conda-linux-gnu (64-bit)
+Running under: CentOS Linux 7 (Core)
+
+Matrix products: default
+BLAS/LAPACK: ~/anaconda3/envs/R.4/lib/libopenblasp-r0.3.20.so
+
+locale:
+[1] C
+
+attached base packages:
+[1] stats4    stats     graphics  grDevices utils     datasets  methods
+[8] base
+
+other attached packages:
+ [1] data.table_1.14.2                 coefplot_1.2.8
+ [3] glmnet_4.1-2                      Matrix_1.4-1
+ [5] tibble_3.1.8                      org.Hs.eg.db_3.13.0
+ [7] AnnotationDbi_1.56.2              ggforce_0.3.3
+ [9] pheatmap_1.0.12                   viridisLite_0.4.0
+[11] simpleCache_0.4.2                 LOLA_1.22.0
+[13] transcripTools_0.0.0.9000         dplyr_1.0.9
+[15] sesame_1.10.5                     sesameData_1.10.1
+[17] rmarkdown_2.14                    ExperimentHub_2.0.0
+[19] AnnotationHub_3.0.2               BiocFileCache_2.2.1
+[21] dbplyr_2.2.1                      edgeR_3.34.1
+[23] limma_3.48.3                      GEOquery_2.60.0
+[25] Biobase_2.54.0                    ggplot2_3.3.6
+[27] reshape2_1.4.4                    purrr_0.3.4
+[29] BSgenome.Hsapiens.UCSC.hg19_1.4.3 BSgenome_1.60.0
+[31] rtracklayer_1.52.1                Biostrings_2.62.0
+[33] XVector_0.34.0                    GenomicRanges_1.44.0
+[35] GenomeInfoDb_1.30.1               IRanges_2.28.0
+[37] S4Vectors_0.32.3                  BiocGenerics_0.40.0
+[39] multcomp_1.4-19                   TH.data_1.1-1
+[41] MASS_7.3-58                       survival_3.3-1
+[43] kableExtra_1.3.4                  mvtnorm_1.1-3
+[44] lme4_1.1-30     
